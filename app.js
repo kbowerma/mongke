7f8b3d479611602ae8dd9fb91fcf43787ac17891
Kyle Bowerman <kyle.bowerman@appirio.com>
2015-01-27 12:00:28 -0600

    added procfile and started app.js as worker.

---
+++ app.js
@@ -0,0 +1,15 @@
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('mongke', 'postgres', 'ayhtfifi', {
+      dialect: "postgres", // or 'sqlite', 'postgres', 'mariadb'
+      port:    5432, // or 5432 (for postgres)
+    })
+
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database:', err)
+    } else {
+      console.log('Connection has been established successfully.')
+    }
+  })
